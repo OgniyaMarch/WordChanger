e11e2ffeb859044271ab963c60472c4a22039921
OgniyaMarch <angel13osetrova@gmail.com>
2023-07-27 19:51:21 +0300

    Add general functionality to convert english to ukrainian languages (with space ignore)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 /*
  * Головна мета створення програми 
@@ -11,8 +12,26 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 			System.setOut(new PrintStream(System.out, true, "UTF-8"));
+	
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.println("Enter a string: ");
+			StringBuilder userString = new StringBuilder(scanner.nextLine()); 
+			
+			VocabularyEnglishUkrainian voc1 = new VocabularyEnglishUkrainian();
+			voc1.changingEnglishToUkrainian(userString);
 		
-			UKtoEN.addToHashMap();
+			
+			scanner.close();
 	}
 
 }
+
+
+/*
+ * Create function for personality vocablurary every user
+ *   // Method to add elements to the HashMap
+    public void addToHashMap(String key, int value) {
+        myHashMap.put(key, value);
+    }
+ * */
---
+++ src/VocabularyEnglishUkrainian.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class VocabularyEnglishUkrainian{
+	//create static HashMap because we don't change it
+	//Create vocabulary for symbols "eng-ukr"
+	//key is english symbols
+	
+	private Map<Character, Character> vocabularyEngUa = new HashMap<>();
+
+	public VocabularyEnglishUkrainian(){
+		//the first line of keyboard
+				vocabularyEngUa.put('q', 'й');
+				vocabularyEngUa.put('w', 'ц');
+				vocabularyEngUa.put('e', 'у');
+				
+				vocabularyEngUa.put('w', 'к');
+				vocabularyEngUa.put('t', 'е');
+				vocabularyEngUa.put('y', 'н');
+				
+				vocabularyEngUa.put('u', 'г');
+				vocabularyEngUa.put('i', 'ш');
+				vocabularyEngUa.put('o', 'щ');
+				
+				vocabularyEngUa.put('p', 'з');
+				vocabularyEngUa.put('[', 'х');
+				vocabularyEngUa.put(']', 'ї');
+				
+				//the second line of keyboard
+				vocabularyEngUa.put('a', 'ф');
+				vocabularyEngUa.put('s', 'і');
+				vocabularyEngUa.put('d', 'в');
+				
+				vocabularyEngUa.put('f', 'а');
+				vocabularyEngUa.put('g', 'п');
+				vocabularyEngUa.put('h', 'р');
+				
+				vocabularyEngUa.put('j', 'о');
+				vocabularyEngUa.put('k', 'л');
+				vocabularyEngUa.put('l', 'д');
+				
+				vocabularyEngUa.put(';','ж');
+				vocabularyEngUa.put('\'','є');
+				
+				//the third line of keyboard
+				vocabularyEngUa.put('z','я');
+				vocabularyEngUa.put('x','ч');
+				vocabularyEngUa.put('c','с');
+				
+				vocabularyEngUa.put('v','м');
+				vocabularyEngUa.put('b','и');
+				vocabularyEngUa.put('n','т');
+				
+				vocabularyEngUa.put('m','ь');
+				vocabularyEngUa.put(',','б');
+				vocabularyEngUa.put('.','ю');
+				
+				vocabularyEngUa.put('/','.');
+				vocabularyEngUa.put('\\','ґ');
+				
+	}
+	
+	public void changingEnglishToUkrainian(StringBuilder userWrongString) {
+		StringBuilder userRightString = new StringBuilder();
+		//for (Map.Entry<String, String> entry : vocabularyEngUa.entrySet()) {
+			for (int i = 0; i < userWrongString.length(); i++) {
+				char key = userWrongString.charAt(i);
+						
+				if (vocabularyEngUa.containsKey(key)) {
+					userRightString.append(vocabularyEngUa.get(key));
+				}
+				else {
+					userRightString.append(key);
+				}
+				
+			}
+		System.out.println(userRightString);
+	}
+	
+	
+	
+}
+
+
+
